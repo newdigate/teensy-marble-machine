58a24bf5a465018af8750b3c42e41f39b408c696
Newdigate <nicnewdigate@gmail.com>
2021-07-15 14:16:02 +0100

    display wilson :) bmp

--- marblemachine.ino
+++ marblemachine.ino
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <MIDI.h>
 #include <Audio.h>
-#include "playsdwavresmp.h"
 #include "sampler.h"
 #include "flashloader.h"
 #include "midireader.h"
@@ -9,6 +8,8 @@
 #include "polyphonicsampler.h"
 #include "tempo.h"
 #include "TeensyVariablePlayback.h"
+#include <ST7735_t3.h> // Hardware-specific library
+#include "TFTPianoDisplay.h"
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 #define KEY_NOTENUMBER_C5 84
@@ -49,8 +50,6 @@ AudioMixer4              mixer1;         //xy=758,280
 AudioMixer4              mixer4;         //xy=772,791
 AudioMixer4              mixer5;         //xy=924,256
 AudioMixer4              mixer6;         //xy=934,422
-AudioEffectReverb        reverb1;        //xy=1057,300
-AudioEffectReverb        reverb2;        //xy=1063,455
 AudioMixer4              mixer7;         //xy=1194,226
 AudioMixer4              mixer8;         //xy=1209,415
 AudioOutputTDM           tdm1;           //xy=1377,352
@@ -90,19 +89,25 @@ AudioConnection          patchCord33(mixer3, 0, mixer5, 1);
 AudioConnection          patchCord34(mixer2, 0, mixer6, 0);
 AudioConnection          patchCord35(mixer1, 0, mixer5, 0);
 AudioConnection          patchCord36(mixer4, 0, mixer6, 1);
-AudioConnection          patchCord37(mixer5, reverb1);
-AudioConnection          patchCord38(mixer5, 0, mixer7, 0);
-AudioConnection          patchCord39(mixer6, reverb2);
-AudioConnection          patchCord40(mixer6, 0, mixer8, 0);
-AudioConnection          patchCord41(reverb1, 0, mixer7, 1);
-AudioConnection          patchCord42(reverb2, 0, mixer8, 1);
-AudioConnection          patchCord43(mixer7, 0, tdm1, 0);
-AudioConnection          patchCord44(mixer7, 0, tdm1, 4);
-AudioConnection          patchCord45(mixer8, 0, tdm1, 2);
-AudioConnection          patchCord46(mixer8, 0, tdm1, 6);
+AudioConnection          patchCord37(mixer5, 0, mixer7, 0);
+AudioConnection          patchCord38(mixer6, 0, mixer8, 0);
+AudioConnection          patchCord39(mixer7, 0, tdm1, 0);
+AudioConnection          patchCord40(mixer7, 0, tdm1, 4);
+AudioConnection          patchCord41(mixer8, 0, tdm1, 2);
+AudioConnection          patchCord42(mixer8, 0, tdm1, 6);
 AudioControlCS42448      cs42448;      //xy=821,899
 // GUItool: end automatically generated code
 #pragma endregion
+
+#pragma region tft
+#define TFT_CS   6  // CS & DC can use pins 2, 6, 9, 10, 15, 20, 21, 22, 23
+#define TFT_DC    2  //  but certain pairs must NOT be used: 2+10, 6+9, 20+23, 21+22
+#define TFT_RST   255 // RST can use any pin
+ST7735_t3 tft = ST7735_t3(TFT_CS, TFT_DC, TFT_RST);
+#pragma endregion
+
+TFTPianoDisplay pianoDisplay1(tft, 3, 4, 4, 0); //tft, byte octaves, byte startOctave, byte x, byte y
+TFTPianoDisplay pianoDisplay2(tft, 3, 7, 4, 13); //tft, byte octaves, byte startOctave, byte x, byte y
 
 sampler                 _sampler;
 newdigate::audiosample  *sample1, *sample2, *sample3, *sample4, *sample5;
@@ -119,7 +124,11 @@ void handleNoteOff(uint8_t channel, uint8_t pitch, uint8_t velocity)
     Serial.printf("off %d %d\n", channel, pitch);
 }
 
-tempo _tempo(120.0f);
+void bmpDraw(const char *filename, uint8_t x, uint16_t y);
+uint16_t read16();
+uint32_t read32();
+
+tempo _tempo(140.0f);
 multisequencer _multisequencer(_tempo);
 midisequenceadapter _adapter(_multisequencer); // to read midi files into the a sequencer pattern
 sequencer *_sequencer;
@@ -128,36 +137,28 @@ AudioPlayArrayResmp *voices[] = {&voice1, &voice2, &voice3, &voice4, &voice5, &v
 
 void setup() {
     Serial.begin(9600);
-    //while(!Serial) {
-    // delay(100);
-    //}
-    delay(100);
-    Serial.print("starting...\n");
-    Serial.flush();
-    
-    delay(200);
 
-    Serial.print("Initializing SD card...");
-    Serial.flush();
+    tft.initR(INITR_GREENTAB);
+    tft.setRotation(3);
+    tft.fillScreen(ST7735_BLACK);
+    tft.println("loading...");
     
     while (!SD.begin(BUILTIN_SDCARD)) {
-      Serial.println("initialization failed!");
+      tft.println("SD initialization failed!");
       delay(1000);
     }
-    Serial.println("initialization done.");
-    Serial.flush();
-    
+    tft.println("SD initialised.");
+
     for (unsigned int i=0; i<8; i++) {
         voices[i]->setNumChannels(2);
         voices[i]->enableInterpolation(true);
         voices[i]->begin();
     }
 
-    reverb1.reverbTime(0.01);
-    reverb2.reverbTime(0.01);
 
-    mixer7.gain(1, 0.25);
-    mixer8.gain(1, 0.25);
+    mixer7.gain(0, 0.35);
+    mixer8.gain(0, 0.35);
+    
     
     envelope1.attack(0);
     envelope2.attack(0);
@@ -179,10 +180,19 @@ void setup() {
 
     newdigate::flashloader loader;
     sample1 = loader.loadSample("PNO1C1.RAW");
+    tft.println("C1");
+
     sample2 = loader.loadSample("PNO1C2.RAW");
+    tft.println("C2");
+
     sample3 = loader.loadSample("PNO1C3.RAW");
+    tft.println("C3");
+
     sample4 = loader.loadSample("PNO1C4.RAW");
+    tft.println("C4");
+
     sample5 = loader.loadSample("PNO1C5.RAW");
+    tft.println("C5");
     
     _sampler.addSample(KEY_NOTENUMBER_C1, sample1->sampledata, sample1->samplesize / 2);
     _sampler.addSample(KEY_NOTENUMBER_C2, sample2->sampledata, sample2->samplesize / 2);
@@ -201,14 +211,10 @@ void setup() {
     cs42448.enable();
     cs42448.volume(1.0f);
 
-    delay(10);
-    Serial.println("Teensy polyphonic sampling demo... Connect your MIDI input cable ");
+    tft.fillScreen(ST7735_BLACK);
+    tft.setCursor(0, 26);
+    tft.println("  Wintergatan\n  Marble machine....");
 
-    /*
-    sequencer *_tempoSequencer = _multisequencer.getTempoSequencer();
-    _adapter.loadMidi("marbles.mid");
-    _adapter.loadMidiTempoTrack(0, 64, 0); // track 0, 64 * 4 beats, offset by 0 bars
-    */
     _sequencer = _multisequencer.newSequencer();
 
     _adapter.loadMidi("marbles.mid");
@@ -219,14 +225,36 @@ void setup() {
     _sequencer->setNextPattern(0);  
 
     _sequencer->onevent = [] (sequencerevent *event) { 
+
         _sampler.noteEvent(event->noteNumber, event->velocity, event->isNoteStartEvent, false);
+        if (event->isNoteStartEvent) {
+            pianoDisplay1.keyDown(event->noteNumber);
+            pianoDisplay2.keyDown(event->noteNumber);
+        } else {
+            pianoDisplay1.keyUp(event->noteNumber);
+            pianoDisplay2.keyUp(event->noteNumber);
+        } 
     };  
+
+
+    if (SD.exists("wilson.jpg")) {
+        Serial.println("drawing wilson...");
+        File wilson = SD.open("wilson.jpg");
+        bmpDraw("wilson.bmp", 0, 45);
+    } else 
+      Serial.println("wilson not found...");
+  
 }
 
 void loop() {
+  
     _multisequencer.tick(micros());
     MIDI.read();
-    //delay(10);
+    if (pianoDisplay1.displayNeedsUpdating())
+        pianoDisplay1.drawPiano();
+    if (pianoDisplay2.displayNeedsUpdating())
+        pianoDisplay2.drawPiano();
+        
 }
 
 namespace std {
@@ -236,3 +264,154 @@ namespace std {
 
 unsigned __exidx_start;
 unsigned __exidx_end;
+
+File bmpFile;
+#define BUFFPIXEL 20
+void bmpDraw(const char *filename, uint8_t x, uint16_t y) {
+
+
+    int      bmpWidth, bmpHeight;   // W+H in pixels
+    uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+    uint32_t bmpImageoffset;        // Start of image data in file
+    uint32_t rowSize;               // Not always = bmpWidth; may have padding
+    uint8_t  sdbuffer[3 * BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+    uint16_t buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+    boolean  goodBmp = false;       // Set to true on valid header parse
+    boolean  flip    = true;        // BMP is stored bottom-to-top
+    int      w, h, row, col;
+    uint8_t  r, g, b;
+    uint32_t pos = 0, startTime = millis();
+
+    uint16_t awColors[320];  // hold colors for one row at a time...
+
+    if ((x >= tft.width()) || (y >= tft.height())) return;
+
+    Serial.println();
+    Serial.print("Loading image '");
+    Serial.print((char *) filename);
+    Serial.print("\'\n");
+
+    // Open requested file on SD card
+    bmpFile = SD.open(filename);
+    if (!bmpFile) {
+        Serial.print("File not found");
+        tft.fillScreen(ST7735_BLACK);
+        tft.setCursor(12, tft.height() / 2 - 12);
+        tft.print("Unable to");
+        tft.setCursor(12, tft.height() / 2 - 0);
+        tft.print("read file: ");
+        tft.setCursor(12, tft.height() / 2 + 12);
+        tft.setTextColor(ST7735_YELLOW);
+        tft.print(filename);
+        return;
+    }
+
+    // Parse BMP header
+    if (read16() == 0x4D42) { // BMP signature
+        Serial.print("File size: ");
+        uint fileSize = read32();
+        Serial.printf("%u \n", fileSize);
+        (void)read32(); // Read & ignore creator bytes
+        bmpImageoffset = read32(); // Start of image data
+        Serial.printf("Image Offset: ");
+        Serial.printf("%u \n", bmpImageoffset);
+        // Read DIB header
+        Serial.printf("Header size: %u\n", read32());
+        bmpWidth  = read32();
+        bmpHeight = read32();
+        if (read16() == 1) { // # planes -- must be '1'
+            bmpDepth = read16(); // bits per pixel
+            Serial.print("Bit Depth: "); Serial.print(bmpDepth);
+            if ((bmpDepth == 24) && (read32() == 0)) { // 0 = uncompressed
+
+                goodBmp = true; // Supported BMP format -- proceed!
+                Serial.printf("Image size: %u x %u\n", bmpWidth, bmpHeight);
+
+                // BMP rows are padded (if needed) to 4-byte boundary
+                rowSize = (bmpWidth * 3 + 3) & ~3;
+
+                // If bmpHeight is negative, image is in top-down order.
+                // This is not canon but has been observed in the wild.
+                if (bmpHeight < 0) {
+                    bmpHeight = -bmpHeight;
+                    flip      = false;
+                }
+
+                // Crop area to be loaded
+                w = bmpWidth;
+                h = bmpHeight;
+                if ((x + w - 1) >= tft.width())  w = tft.width()  - x;
+                if ((y + h - 1) >= tft.height()) h = tft.height() - y;
+
+                for (row = 0; row < h; row++) { // For each scanline...
+
+                    // Seek to start of scan line.  It might seem labor-
+                    // intensive to be doing this on every line, but this
+                    // method covers a lot of gritty details like cropping
+                    // and scanline padding.  Also, the seek only takes
+                    // place if the file position actually needs to change
+                    // (avoids a lot of cluster math in SD library).
+                    if (flip) // Bitmap is stored bottom-to-top order (normal BMP)
+                        pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+                    else     // Bitmap is stored top-to-bottom
+                        pos = bmpImageoffset + row * rowSize;
+                    if (bmpFile.position() != pos) { // Need seek?
+                        bmpFile.seek(pos);
+                        buffidx = sizeof(sdbuffer); // Force buffer reload
+                    }
+
+                    for (col = 0; col < w; col++) { // For each pixel...
+                        // Time to read more pixel data?
+                        if (buffidx >= sizeof(sdbuffer)) { // Indeed
+                            bmpFile.read(sdbuffer, sizeof(sdbuffer));
+                            buffidx = 0; // Set index to beginning
+                        }
+
+                        // Convert pixel from BMP to TFT format, push to display
+#ifdef IMAGE_COLORS_BGR
+                        b = sdbuffer[buffidx++];
+            g = sdbuffer[buffidx++];
+            r = sdbuffer[buffidx++];
+#else
+                        r = sdbuffer[buffidx++];
+                        g = sdbuffer[buffidx++];
+                        b = sdbuffer[buffidx++];
+#endif
+                        awColors[col] = tft.Color565(r, g, b);
+                    } // end pixel
+                    tft.writeRect(0, row + y, w, 1, awColors);
+                } // end scanline
+                Serial.print("Loaded in ");
+                Serial.print(millis() - startTime);
+                Serial.print(" ms\n");
+            } // end goodBmp
+        }
+    }
+
+    bmpFile.close();
+    if (!goodBmp) Serial.print("BMP format not recognized.\n");
+}
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16() {
+    uint16_t result = 0;
+    ((uint8_t *)&result)[0] = bmpFile.read(); // LSB
+    ((uint8_t *)&result)[1] = bmpFile.read(); // MSB
+    return result;
+}
+
+uint32_t read32() {
+    uint32_t result = 0;
+    uint8_t x1 = (uint8_t)bmpFile.read();
+    uint8_t x2 = (uint8_t)bmpFile.read();
+    uint8_t x3 = (uint8_t)bmpFile.read();
+    uint8_t x4 = (uint8_t)bmpFile.read();
+    ((uint8_t *)&result)[0] = x1; // LSB
+    ((uint8_t *)&result)[1] = x2;
+    ((uint8_t *)&result)[2] = x3;
+    ((uint8_t *)&result)[3] = x4; // MSB
+    return result;
+}
